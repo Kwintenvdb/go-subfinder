137dcfeb9276726216f5f025515b14fd2edcdc8b
Kwinten Van den Berghe <kwintenvdberghe@gmail.com>
2022-05-13 22:55:44 +0200

    Added CLI package, video file finding

--- main.go
+++ main.go
@@ -2,23 +2,58 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/Kwintenvdb/go-subfinder/client"
+	mediafinder "github.com/Kwintenvdb/go-subfinder/media_finder"
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-func main() {
-	setupConfig()
+var rootCommand = &cobra.Command{
+	Use: "go-subfinder",
+	Run: func(cmd *cobra.Command, args []string) {
+		println("executing command")
+	},
+}
 
-	client := client.New(client.ClientConfig{
-		ApiKey: viper.GetString("apiKey"),
-		Username: viper.GetString("username"),
-		Password: viper.GetString("password"),
-	})
-	client.Login()
-	subs := client.FindSubtitles("tokyo.vice.s01e08.1080p.web.h264-cakes.mkv")
-	subId := subs.Data[0].Attributes.Files[0].FileId
-	client.DownloadSubtitle(subId)
+var downloadCommand = &cobra.Command{
+	Use: "download",
+	Run: func(cmd *cobra.Command, args []string) {
+		println("executing download command")
+
+		println("Listing files in dir")
+		video, err := mediafinder.FindVideoFileInCurrentDir()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Found video file: %s\n", video)
+
+		client := client.New(client.ClientConfig{
+			ApiKey:   viper.GetString("apiKey"),
+			Username: viper.GetString("username"),
+			Password: viper.GetString("password"),
+		})
+		client.Login()
+		subs := client.FindSubtitles(video)
+		subId := subs.Data[0].Attributes.Files[0].FileId
+		client.DownloadSubtitle(subId)
+	},
+}
+
+func ExecuteRootCommand() {
+	if err := rootCommand.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func main() {
+	rootCommand.AddCommand(downloadCommand)
+
+	cobra.OnInitialize(setupConfig)
+	ExecuteRootCommand()
 }
 
 func setupConfig() {
---
+++ media_finder/find_media.go
@@ -0,0 +1,95 @@
+package mediafinder
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+)
+
+var (
+	videoFileExtensions = []string{
+		"webm",
+		"mkv",
+		"flv",
+		"vob",
+		"ogv",
+		"ogg",
+		"rrc",
+		"gif",
+		"mng",
+		"mov",
+		"avi",
+		"qt",
+		"wmv",
+		"yuv",
+		"rm",
+		"asf",
+		"amv",
+		"mp4",
+		"m4p",
+		"m4v",
+		"mpg",
+		"mp2",
+		"mpeg",
+		"mpe",
+		"mpv",
+		"m4v",
+		"svi",
+		"3gp",
+		"3g2",
+		"mxf",
+		"roq",
+		"nsv",
+		"flv",
+		"f4v",
+		"f4p",
+		"f4a",
+		"f4b",
+		"mod",
+	}
+)
+
+func FindVideoFileInCurrentDir() (fileName string, err error) {
+	files := getFilesInCurrentDir()
+
+	videoFiles := make([]fs.FileInfo, 0)
+	for _, f := range files {
+		fmt.Println(f.Name())
+		if isVideoFile(f) {
+			videoFiles = append(videoFiles, f)
+		}
+	}
+
+	numVideoFiles := len(videoFiles)
+
+	if numVideoFiles == 0 {
+		return "", errors.New("no video file found in current dir")
+	}
+
+	if numVideoFiles > 1 {
+		return "", fmt.Errorf("found more than 1 video file. Found %d", numVideoFiles)
+	}
+
+	return videoFiles[0].Name(), nil
+}
+
+func getFilesInCurrentDir() []fs.FileInfo {
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return files
+}
+
+func isVideoFile(file fs.FileInfo) bool {
+	for _, ext := range videoFileExtensions {
+		fileExt := filepath.Ext(file.Name())
+		if ext == fileExt {
+			return true
+		}
+	}
+	return false
+}
